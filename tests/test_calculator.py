093dcc4ba87e8e7ebf9a18b7d861d30c00ba0f3b
so338njit <so338@njit.edu>
2025-02-03 11:33:29 -0500

    added the virtual enviroment folder, installed requirements.txt, and added calculator and test functions in __init__ files

---
+++ tests/test_calculator.py
@@ -0,0 +1,19 @@
+'''My Calculator Test'''
+from calculator import add, subtract, divide, multiply
+
+def test_addition():
+    '''tests that the add function works'''
+    assert add(2,2) == 4
+
+def test_subtraction():
+    '''tests that the subtract function works'''
+    assert subtract(2,2) == 0
+
+def test_divide():
+    '''tests that the divide function works'''
+    assert divide(2,2) == 1
+
+    def test_multiply():
+        '''tests that the add function works'''
+        assert multiply(2,2) == 4
+        
