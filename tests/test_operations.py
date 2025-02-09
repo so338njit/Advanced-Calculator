36a428405243a0ef71ecac5ef00825a2a5c101ef
so338njit <so338@njit.edu>
2025-02-09 08:52:34 -0500

    test operations file added code to test functionality of operations

---
+++ tests/test_operations.py
@@ -0,0 +1,31 @@
+"""testing operations"""
+from decimal import Decimal
+import pytest
+from calculator.calculation import Calculation
+from calculator.operations import add, subtract, multiply, divide
+
+def test_operation_add():
+    """testing the addition opertion"""
+    calculation = Calculation(Decimal('10'), Decimal('5'), add)
+    assert calculation.perform() == Decimal('15'), "Add operation failed"
+
+def test_operation_subtract():
+    """testing the subtraction operation"""
+    calculation = Calculation(Decimal('10'), ('5'), subtract)
+    assert calculation.perform() == Decimal('5'), "Subtract operation failed"
+
+def test_operation_multiply():
+    """testing the multiplication operation"""
+    calculation = Calculation(Decimal('2'), ('5'), multiply)
+    assert calculation.perform() == Decimal('10'), "Multiply operation failed"
+
+def test_operation_divide():
+    """testing the divide operation"""
+    calculation = Calculation(Decimal('10'), Decimal('5'), divide)
+    assert calculation.perform() == Decimal('2'), "Divide operation failed"
+
+def test_divide_by_zero():
+    """testing the exception to divide by zero"""
+    with pytest.raises(ValueError, match="Cannot divide by zero"):
+        calculation = Calculation(Decimal('10'), Decimal('0'), divide)
+        calculation.perform()
