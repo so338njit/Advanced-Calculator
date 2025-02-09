023229229299afd175bbb79331396075957e8885
so338njit <so338@njit.edu>
2025-02-09 08:51:00 -0500

    test calculation file updated for pytest edited for OOP

---
+++ tests/test_calculation.py
@@ -0,0 +1,29 @@
+# pylint: disable=unnecessary-dunder-call, invalid-name
+from decimal import Decimal
+import pytest
+from calculator.calculation import Calculation
+from calculator.operations import add, subtract, multiply, divide
+
+@pytest.mark.paramertize("a, b, operation, expected", [
+    (Decimal('10'), Decimal('5'), add, Decimal('15')),
+    (Decimal('10'), Decimal('5'), subtract, Decimal('5')),
+    (Decimal('10'), Decimal('2'), multiply, Decimal('20')),
+    (Decimal('10'), Decimal('2'), divide, Decimal('5')),
+    (Decimal('10.5'), Decimal('1.5'), add, Decimal('12')),
+    (Decimal('10.7'), Decimal('0.9'), subtract, Decimal('11.8')),
+    (Decimal('10.5'), Decimal('2'), multiply, Decimal('21')),
+    (Decimal('10'), Decimal('0,5'), divide, Decimal('20')),
+])
+def test_calculation_operations(a, b, operation, expected):
+    calc = Calculation(a, b, operation)
+    assert calc.perform() == expected, f"Failed {operation.__name__} operations with {a} and {b}"
+
+def test_calculation_repr():
+    calc = Calculation(Decimal('10'), Decimal('5'), add)
+    expected_repr = "Calculation(10, 5, add)"
+    assert calc.__repr__() == expected_repr, "The __repr__ method output does not match the expected string."
+
+def test_divide_by_zreo():
+    calc = Calculation(Decimal('10'), Decimal('0'), divide)
+    with pytest.raised(ValueError, match="Cannot divide by zero"):
+        calc.perfrom()
