2b7200b611f829e083ec243685166890f828d9ae
so338njit <so338@njit.edu>
2025-02-11 22:50:01 -0500

    replaced single operations test with a test that covers all operations

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -4,25 +4,10 @@ import pytest
 from calculator.calculation import Calculation
 from calculator.operations import add, subtract, multiply, divide
 
-def test_operation_add():
-    """testing the addition opertion"""
-    calculation = Calculation(Decimal('10'), Decimal('5'), add)
-    assert calculation.perform() == Decimal('15'), "Add operation failed"
-
-def test_operation_subtract():
-    """testing the subtraction operation"""
-    calculation = Calculation(Decimal('10'), Decimal('5'), subtract)
-    assert calculation.perform() == Decimal('5'), "Subtract operation failed"
-
-def test_operation_multiply():
-    """testing the multiplication operation"""
-    calculation = Calculation(Decimal('2'), Decimal('5'), multiply)
-    assert calculation.perform() == Decimal('10'), "Multiply operation failed"
-
-def test_operation_divide():
-    """testing the divide operation"""
-    calculation = Calculation(Decimal('10'), Decimal('5'), divide)
-    assert calculation.perform() == Decimal('2'), "Divide operation failed"
+def test_operation(a, b, operation, expected)
+    """test covering all operations"""
+    calculation = Calculation.create(a, b, operation, expected)
+    assert calculation.perform() == expected, f"{operation.__name__} operation failed"
 
 def test_divide_by_zero():
     """testing the exception to divide by zero"""
